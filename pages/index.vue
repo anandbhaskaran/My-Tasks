2cbbe7adf4521aab977d7b9a59a5d0f7788fa1fa
Anand <anand.bhaskaran@beekeeper.io>
2022-07-22 09:46:31 +0200

    My First commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+  <DarkSidebar current="Dashboard"/>
+
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
---
+++ pages/myTasks.vue
@@ -0,0 +1,11 @@
+<template>
+  <DarkSidebar current="My Tasks"/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
